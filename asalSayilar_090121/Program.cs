1040d66eace417bf1b2e9d4352547c49384ec005
Merve <60087899+mervesurer@users.noreply.github.com>
2021-01-10 02:37:19 +0300

    Add files via upload

---
+++ asalSayilar_090121/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalSayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int sayi = 2; sayi <= 10000; sayi++)
+            {
+                bool asalMi = true; 
+
+                for (int bolen = 2; bolen <= sayi / 2; bolen++)
+                {
+                    if (sayi % bolen == 0)
+                    {
+                        asalMi = false;
+                        break;
+                    }
+                }
+
+                if (asalMi)
+                {
+                    Console.WriteLine($"{sayi} asaldır!!");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
